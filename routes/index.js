4ed8715b05594ba6a1f7be2faf1cc4e258a328e5
carey <schiafang@gmail.com>
2020-09-12 15:07:58 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const userController = require('../controllers/userControllers')
 
 module.exports = (app, passport) => {
-  app.get('/', (req, res) => res.render(index))
+  app.get('/', (req, res) => res.render('index'))
   app.post('/api/signup', userController.signUp)
   app.post('/api/signin', userController.signIn)
 }
