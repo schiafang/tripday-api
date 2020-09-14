ffe5733ffb400068857a1a67528d3fa98a9543f9
carey <schiafang@gmail.com>
2020-09-14 15:41:12 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const userController = require('../controllers/userControllers')
-const passport = require('../config/passport')
-
-const authenticated = passport.authenticate('jwt', { session: false })
 
 module.exports = (app, passport) => {
+
+  function authenticated(req, res, next) {
+    passport.authenticate('jwt', { session: false }, (error, user, i) => {
+      if (!user) { return res.status(401).json({ status: 'error', message: "驗證失敗" }) }
+      req.user = user
+      return next()
+    })(req, res, next)
+  }
+
   app.get('/', (req, res) => res.render('index'))
 
   app.get('/get_current_user', authenticated, userController.getCurrentUser)
